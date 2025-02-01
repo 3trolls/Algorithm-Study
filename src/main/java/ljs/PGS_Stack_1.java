2584323548e2b4864bcdc892cb6224473b405f7b
ijs <js230023@gmail.com>
2025-02-01 21:09:54 +0900

    #13 Feat : PSG_같은숫자는싫어

---
+++ src/main/java/ljs/PGS_Stack_1.java
@@ -0,0 +1,26 @@
+package ljs;
+
+
+import java.util.ArrayList;
+
+public class PGS_Stack_1 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {1, 1, 3, 3, 0, 1, 1};
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<>();
+        int del = -1;        
+        int[] answer ;
+        for(int i = 0 ;i < arr.length ;i++){            
+            if(arr[i] != del){
+                list.add(arr[i]);
+                del = arr[i];
+            }
+            
+        }
+        answer = list.stream().mapToInt(Integer::intValue).toArray();        
+
+        System.out.println(answer);
+	}
+
+}
